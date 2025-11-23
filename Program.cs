d0f162816fcd3140ea2f22fdf71f92ba8fef311d
Ivo Petrov <Ivo.Petrov@ascent.io>
2025-11-23 14:58:41 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DebugExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int score = 85;
+            if (score > 90)
+            {
+                Console.WriteLine("Great Job");
+            }
+            else
+            {
+                Console.WriteLine("Try again!");
+            }
+        }
+    }
+}
